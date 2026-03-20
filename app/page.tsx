81aed750cb0ae092164f4a7341ab42e17ca90bbe
Agustina Mena <agusmena60@gmail.com>
2026-03-20 12:09:54 -0300

    feat: mejoras en el seo, footer, navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,18 @@
 import Hero from "./components/Hero";
 import Services from "./components/Services";
 import CTA from "./components/CTA";
+import FadeIn from "./components/FadeIn";
 
 export default function Home() {
   return (
     <main className="bg-white text-gray-800">
       <Hero />
-      <Services />
-      <CTA />
+      <FadeIn>
+        <Services />
+      </FadeIn>
+      <FadeIn>
+        <CTA />
+      </FadeIn>
     </main>
   );
 }
