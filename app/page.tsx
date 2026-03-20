5d50878cb47a14342187b41353360e72f2f4364c
Agustina Mena <agusmena60@gmail.com>
2026-03-20 11:10:16 -0300

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -2,64 +2,96 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <main className="bg-white text-gray-800">
+      <section className="min-h-screen flex flex-col justify-center items-center text-center px-6 bg-linear-to-b from-lime-50 to-white">
+        <h1 className="text-4xl md:text-6xl font-bold mb-4 leading-tight">
+          Realzá tu belleza{" "}
+          <span
+            className="text-[#2D4517]
+"
           >
+            natural
+          </span>
+        </h1>
+
+        <p className="text-lg md:text-xl mb-6 max-w-xl text-gray-600">
+          Tratamientos estéticos personalizados para que te sientas segura,
+          cómoda y radiante.
+        </p>
+
+        <div className="flex gap-4">
+          <button className="btn-spa">Reservar turno</button>
+
+          <button
+            className="btn-spa-outline"
+          >
+            Ver servicios
+          </button>
+        </div>
+      </section>
+      <section className="py-16 px-6 text-center">
+        <h2 className="text-3xl font-semibold mb-10">Nuestros servicios</h2>
+
+        <div className="grid md:grid-cols-3 gap-6">
+          <div className="shadow rounded-xl overflow-hidden">
             <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
+              src="/cleaning.jpg"
+              alt="Limpieza facial"
+              width={400}
+              height={300}
+              className="w-full h-48 object-cover"
             />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+            <div className="p-4">
+              <h3 className="font-bold mb-2">Limpieza facial</h3>
+              <p>Mejorá la salud y apariencia de tu piel.</p>
+            </div>
+          </div>
+
+          <div className="shadow rounded-xl overflow-hidden">
+            <Image
+              src="/depilation.jpg"
+              alt="Depilación"
+              width={400}
+              height={300}
+              priority
+              className="w-full h-48 object-cover"
+            />{" "}
+            <div className="p-4">
+              <h3 className="font-bold mb-2">Depilación definitiva</h3>
+              <p>Resultados duraderos y sin dolor.</p>
+            </div>
+          </div>
+
+          <div className="shadow rounded-xl overflow-hidden">
+            <Image
+              src="/esthetic.jpg"
+              alt="Tratamientos corporales"
+              priority
+              width={400}
+              height={300}
+              className="w-full h-48 object-cover"
+            />
+            <div className="p-4">
+              <h3 className="font-bold mb-2">Tratamientos corporales</h3>
+              <p>Modelá y cuidá tu cuerpo.</p>
+            </div>
+          </div>
         </div>
-      </main>
-    </div>
+      </section>
+      <section className="py-16 text-center bg-lime-100">
+        <h2 className="text-3xl font-semibold mb-4">Reservá tu turno hoy</h2>
+        <p className="mb-6">
+          Escribinos por WhatsApp y coordinamos tu consulta.
+        </p>
+        <a
+          href="https://wa.me/549381XXXXXXX?text=Hola!%20quiero%20consultar%20por%20un%20turno"
+          rel="noopener noreferrer"
+          target="_blank"
+          className="btn-spa"
+        >
+          Reservar turno
+        </a>
+      </section>
+    </main>
   );
 }
